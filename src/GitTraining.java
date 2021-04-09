664f14357e4155968f43ba1ca02989b367e13250
Mija Jelonek <mija.jelonek@hm.com>
2021-04-09 12:43:54 +0200

    new class GitTraining

---
+++ src/GitTraining.java
@@ -0,0 +1,13 @@
+public class GitTraining {
+    public static void main(String[] args) {
+        int heltal = 13;
+        int nyttTal = increment(heltal);
+        System.out.println(nyttTal);
+
+    }
+
+    public static int increment(int input) {
+        return input + 1;
+    }
+
+}
